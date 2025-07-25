02ee50c1d93c772167e8e2ebc453098a64605c27
stream0426 <h.ogawa.0426@gmail.com>
2025-07-25 21:44:58 +0900

    Add RBFE_validation directory

---
+++ RBFE_validation/FEP/H_pocket/star_map/fep_csv2tsv.py
@@ -0,0 +1,37 @@
+import csv
+import glob
+import numpy as np
+from collections import defaultdict
+
+csv_files = glob.glob('fep_mapper*.csv')  # 例: 'data1.csv', 'data2.csv', 'data3.csv'
+
+fep_data = defaultdict(list)
+
+for csv_file in csv_files:
+    with open(csv_file, 'r') as f:
+        reader = csv.DictReader(f)
+        
+        for row in reader:
+            ligand_i = int(row['Ligand1'])
+            ligand_j = int(row['Ligand2'])
+            fep_value = float(row['FEP'])
+            
+            fep_data[(ligand_i, ligand_j)].append(fep_value)
+
+results = []
+
+for (ligand_i, ligand_j), fep_values in fep_data.items():
+    fep_mean = np.mean(fep_values)
+    fep_std = np.std(fep_values)
+    
+    if fep_std == 0:
+        fep_std = 0.01
+    
+    results.append((ligand_i, ligand_j, fep_mean, fep_std))
+
+with open('final_results_Hpocket_star_FEP.tsv', 'w', newline='') as tsvfile:
+    writer = csv.writer(tsvfile, delimiter='\t')
+    writer.writerow(['ligand_i', 'ligand_j', 'DDG(i->j) (kcal/mol)', 'uncertainty (kcal/mol)'])
+    
+    for result in results:
+        writer.writerow(result)
