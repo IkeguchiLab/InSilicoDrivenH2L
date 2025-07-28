2473724d7877525c4b0869e387e4fa0fd6d2397f
stream0426 <h.ogawa.0426@gmail.com>
2025-07-28 20:57:48 +0900

    Add Step2

---
+++ Step2/cycle/summary2tsv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv('results_summary_core4_R1_cycle.csv')
+
+ligands = df['edge_run'].str.extract(r'(lig_\d+-?\d*)_(lig_\d+-?\d*)')
+df['ligand_i'] = ligands[0]
+df['ligand_j'] = ligands[1]
+
+df_output = df[['ligand_i', 'ligand_j', 'ddG_mean', 'ddG_standard_dev']].copy()
+df_output.columns = ['ligand_i', 'ligand_j', 'DDG(i->j) (kcal/mol)', 'uncertainty (kcal/mol)']
+
+df_output["uncertainty (kcal/mol)"] = df_output["uncertainty (kcal/mol)"].replace(0, 0.01)
+
+df_output.to_csv("final_results_core4_R1_cycle.tsv", sep='\t', index=False)
